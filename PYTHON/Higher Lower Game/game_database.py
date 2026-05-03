a7e2caf29cccd21a0b57cb63f5d72123b76792b0
shubham kumar <shubhamkumar092204@gmail.com>
2026-05-03 22:23:21 +0530

    Higher Lower Game

---
+++ PYTHON/Higher Lower Game/game_database.py
@@ -0,0 +1,148 @@
+data = [
+    
+    {
+        'name' : " shubham's Lecture" ,
+        'follower_count' : 1 ,
+        'description' : ' Youtuber ' ,
+        'country' : ' India '  
+        
+    },
+    
+    {
+        'name' : " cristiano Ronaldo " ,
+        'follower_count' :  600 ,
+        'description' : ' Footballer ' ,
+        'country' : ' Portugal '  
+        
+    },
+    
+    {
+        'name' : " Narendra Modi " ,
+        'follower_count' : 90 ,
+        'description' : ' Prime Minister' ,
+        'country' : 'India '  
+        
+    },
+    
+    {
+        'name' : " Fit tuber " ,
+        'follower_count' : 7 ,
+        'description' : ' Youtuber ' ,
+        'country' : ' India '  
+        
+    },
+    
+    {
+        'name' : " Selena Gomez " ,
+        'follower_count' :  420 ,
+        'description' : ' Musician and actress ' ,
+        'country' : ' United states '  
+        
+    },
+    
+    {
+        'name' : " Kylie Gener " ,
+        'follower_count' : 400 ,
+        'description' : ' Reality TV personality and Businesswoman and self-made Billionaire ' ,
+        'country' : ' United states '  
+        
+    },
+    
+    {
+        'name' : " Pankaj Tripathi " ,
+        'follower_count' : 5 ,
+        'description' : ' Actor ' ,
+        'country' : ' India '  
+        
+    },
+    
+    {
+        'name' : " Lionel Messi " ,
+        'follower_count' : 480 ,
+        'description' : ' FootBaller ' ,
+        'country' : ' Argentina '  
+        
+    },
+    
+    {
+        'name' : " Beyonce " ,
+        'follower_count' : 315 ,
+        'description' : ' Musician ' ,
+        'country' : ' United States '  
+        
+    },
+    
+    {
+        'name' : " T-series " ,
+        'follower_count' : 245 ,
+        'description' : ' Music Label & movie Studio ' ,
+        'country' : ' Inida '  
+        
+    },
+    
+    {
+        'name' : " Neeraj Chopra " ,
+        'follower_count' : 6 ,
+        'description' : ' Athlete ' ,
+        'country' : ' India '  
+        
+    },
+    
+    {
+        'name' : " Justin Bieber " ,
+        'follower_count' : 293 ,
+        'description' : ' Musician ' ,
+        'country' : ' Canada '  
+        
+    },
+    
+    {
+        'name' : " Taylor Swift " ,
+        'follower_count' : 267 ,
+        'description' : ' Musician ' ,
+        'country' : ' United States '  
+        
+    },
+    
+    {
+        'name' : " Jennifer Lopez " ,
+        'follower_count' : 249 ,
+        'description' : ' Musician and Actress ' ,
+        'country' : ' United States '  
+        
+    },
+    
+    {
+        'name' : " Shakira " ,
+        'follower_count' : 87 ,
+        'description' : ' Musician ' ,
+        'country' : ' Colombia '  
+        
+    },
+    
+    {
+        'name' : " NASA " ,
+        'follower_count' : 94 ,
+        'description' : ' Space Agency ' ,
+        'country' : ' United States '  
+        
+    },
+    
+    {
+        'name' : " ISRO " ,
+        'follower_count' : 1 ,
+        'description' : ' Space Agency ' ,
+        'country' : ' India '  
+        
+    },
+    
+    {
+        'name' : " Virat Kohli " ,
+        'follower_count' : 254 ,
+        'description' : ' Cricketer ' ,
+        'country' : ' India '  
+        
+    },
+    
+
+]
